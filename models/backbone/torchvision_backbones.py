51f993d80ede29b77b17d56507ccaa8eb7251b6c
suyashkumar234 <kumar.suyash2304@gmail.com>
2025-07-30 05:35:07 +0530

    Add ResNet101 encoder support and implement self-supervised contrastive learning
    - Add TVDeeplabRes101Encoder class to torchvision_backbones.py for ResNet101 support
    - Refactor contrastive loss to self-supervised learning without organ class dependency
    - Update SupervisedPixelWiseContrastiveLoss to work with binary masks only
    - Maintain backward compatibility with ContrastiveLoss wrapper class

--- models/backbone/torchvision_backbones.py
+++ models/backbone/torchvision_backbones.py
@@ -46,21 +46,68 @@ class Encoder(nn.Module):
         return self.selected_out['layer4']
 
 
-class TVDeeplabRes101Encoder(nn.Module):
+class TVDeeplabRes50Encoder(nn.Module):
     """
-    FCN-Resnet101 backbone from torchvision deeplabv3
-    No ASPP is used as we found emperically it hurts performance
+    FCN-Resnet50 backbone from torchvision deeplabv3
+    No ASPP is used as we found empirically it hurts performance
     """
     def __init__(self, use_coco_init, aux_dim_keep = 64, use_aspp = False):
         super().__init__()
-        _model = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=use_coco_init, 
+        _model = torchvision.models.segmentation.deeplabv3_resnet50(pretrained=use_coco_init, 
                                                                      progress=True, 
                                                                      num_classes=21, 
                                                                      aux_loss=None)
         if use_coco_init:
-            print("###### NETWORK: Using ms-coco initialization ######")
+            print("###### NETWORK: Using ms-coco initialization (ResNet50) ######")
         else:
-            print("###### NETWORK: Training from scratch ######")
+            print("###### NETWORK: Training from scratch (ResNet50) ######")
+
+        _model_list = list(_model.children())
+        self.aux_dim_keep = aux_dim_keep
+        self.backbone = _model_list[0]
+        self.localconv = nn.Conv2d(2048, 256,kernel_size = 1, stride = 1, bias = False) # reduce feature map dimension
+        self.asppconv = nn.Conv2d(256, 256,kernel_size = 1, bias = False)
+
+        _aspp = _model_list[1][0]
+        _conv256 = _model_list[1][1]
+        self.aspp_out = nn.Sequential(*[_aspp, _conv256] )
+        self.use_aspp = use_aspp
+
+    def forward(self, x_in, low_level = False):
+        """
+        Args:
+            low_level: whether returning aggregated low-level features in FCN
+        """
+        fts = self.backbone(x_in)
+        if self.use_aspp:
+            fts256 = self.aspp_out(fts['out'])
+            high_level_fts = fts256
+        else:
+            fts2048 = fts['out']
+            high_level_fts = self.localconv(fts2048)
+
+        if low_level:
+            low_level_fts = fts['aux'][:, : self.aux_dim_keep]
+            return high_level_fts, low_level_fts
+        else:
+            return high_level_fts
+
+
+class TVDeeplabRes101Encoder(nn.Module):
+    """
+    FCN-Resnet101 backbone from torchvision deeplabv3
+    No ASPP is used as we found empirically it hurts performance
+    """
+    def __init__(self, use_coco_init, aux_dim_keep = 64, use_aspp = False):
+        super().__init__()
+        _model = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=use_coco_init, 
+                                                                      progress=True, 
+                                                                      num_classes=21, 
+                                                                      aux_loss=None)
+        if use_coco_init:
+            print("###### NETWORK: Using ms-coco initialization (ResNet101) ######")
+        else:
+            print("###### NETWORK: Training from scratch (ResNet101) ######")
 
         _model_list = list(_model.children())
         self.aux_dim_keep = aux_dim_keep
